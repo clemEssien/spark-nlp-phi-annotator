1e86f46afda7c79c9b8d645c6115329c6ef6d30d
tschaffter <thomas.schaffter@gmail.com>
2021-07-12 16:52:20 -0700

    Run start-master.sh from docker-entrypoint.sh

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -3,6 +3,9 @@ set -e
 
 if [ "$1" = 'uwsgi' ] || [ "$1" = 'python' ]; then
     cd ${APP_DIR}
+    # Start Apache Spark on port 8080
+    bash start-master.sh
+    # Start the container command
     exec gosu www-data "$@"
 fi
 
